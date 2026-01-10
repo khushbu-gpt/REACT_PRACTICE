6f6940084ba684fb77608feaafcc95525dc3b7a3
khushbu kumari <khushbugpt212005@gmail.com>
2026-01-10 20:23:50 +0530

    api fetch

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [users, setUsers] = useState([]);
 
-
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+console.log(users)
   return (
     <div>
-      <h1>Welcome to my app</h1>
+      <h2>Users</h2>
+      {users.map(user => (
+        <p key={user.id}>{user.name}</p>
+      ))}
     </div>
   );
 }
