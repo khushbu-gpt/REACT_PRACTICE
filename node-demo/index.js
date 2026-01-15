a3f93adfe8dbc1dcd61ca42b8f876acad09ba9a0
khushbu kumari <khushbugpt212005@gmail.com>
2026-01-15 17:07:24 +0530

    file writing

--- node-demo/index.js
+++ node-demo/index.js
@@ -1,7 +1,18 @@
-try {
-  JSON.parse("{name: 'JS'}");
-} catch (e) {
-  console.log("Error caught");
-} finally {
-  console.log("Done");
-}
+const fs = require('fs');
+const http = require("http");
+
+// fs.writeFileSync(`test.css`, 'Hello');
+// console.log('File created');
+
+
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from Node server");
+});
+
+
+
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
