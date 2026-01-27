4d794e106290682da33f90e4a50f2e18f906a059
khushbu kumari <khushbugpt212005@gmail.com>
2026-01-28 00:16:18 +0530

    added productsList component

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,8 +1,10 @@
+import ProductLists from "./components/ProductLists";
 import UserLists from "./components/UserLists"
 export default function App() {
   return (
     <div className="">
       <UserLists/>
+      <ProductLists/>
     </div>
   );
 }
