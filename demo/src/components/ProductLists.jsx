c5ee1faed4b9f84ca6713ecf78b232bb0306b402
khushbu kumari <khushbugpt212005@gmail.com>
2026-01-27 23:58:24 +0530

    feat:created productList page & fetch product list API

---
+++ demo/src/components/ProductLists.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { fetchProduct } from "../api/productApi";
+
+const ProductLists = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    (async function () {
+      try {
+        setLoading(true);
+        const res = await fetchProduct();
+        setProducts(res.data);
+        console.log(res);
+      } catch (err) {
+        setError(err.response?.data?.message);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  if (loading) return <div>Loadng...</div>;
+  if (error) return console.log(error);
+  return (
+    <>
+    <h1 className="text-4xl font-semibold text-center  ">Our Products</h1>
+    <div className="flex gap-4 flex-wrap my-4 justify-center ">
+      {products?.map((product) => (
+        <div>
+           <img src={product.category.image} height={250} width={250}/>
+          <p>{product.title}</p>
+          {/* <p>{product.description}</p> */}
+        </div>
+      ))}
+    </div>
+    </>
+  );
+};
+
+export default ProductLists;
