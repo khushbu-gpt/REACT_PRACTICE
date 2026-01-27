926f21e33de5aad8539678c0ba404b5e0d6516a8
khushbu kumari <khushbugpt212005@gmail.com>
2026-01-27 22:56:12 +0530

    feat:made custom hooks

--- demo/src/hooks/useFetch.js
+++ demo/src/hooks/useFetch.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import api from "../api/axiosInstance";
 
-const useFetch = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    api.get(url).then(res => setData(res.data));
+  }, [url]);
+
+  return data;
+};
 
 export default useFetch
