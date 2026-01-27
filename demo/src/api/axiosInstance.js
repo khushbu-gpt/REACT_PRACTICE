0d741ed43dc0db5e040e6d19494711df4cda39c3
khushbu kumari <khushbugpt212005@gmail.com>
2026-01-27 22:24:03 +0530

    feat:created axios response,request interceptor

--- demo/src/api/axiosInstance.js
+++ demo/src/api/axiosInstance.js
@@ -1,7 +1,31 @@
-import axios from "axios"
+import axios from "axios";
 
-export const instance=axios.create({
-baseURL:"https://api.escuelajs.co/api/v1",
-timeout:1000,
-withCredentials:true
-})
+const api = axios.create({
+  baseURL: "https://api.escuelajs.co/api/v1",
+  timeout: 1000,
+  withCredentials: true,
+});
+
+// Request Interceptor (token auto add)
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error)
+  },
+);
+export default api;
