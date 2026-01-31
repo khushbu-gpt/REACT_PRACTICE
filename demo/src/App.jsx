58e6ab7e7a5943b6e5eb50a4d9c770d71a43952c
khushbu kumari <khushbugpt212005@gmail.com>
2026-01-31 23:38:57 +0530

    feat: implement tab navigation with active state management

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,12 +1,21 @@
-import Login from "./components/Login";
-import ProductLists from "./components/ProductLists";
-import UserLists from "./components/UserLists"
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const tabs=["tab1","tab2","tab3"]
+  const [active,setActive]=useState(tabs[0])
+
+  const handleNavigate=(tab)=>{
+    setActive(tab)
+  }
+  useEffect(()=>{
+    console.log(active)
+  },[active])
   return (
-    <div className="">
-      <Login/>
-      {/* <UserLists/> */}
-      {/* <ProductLists/> */}
+    <div className="flex gap-4">
+      {tabs.map((tab)=>{
+        return <button onClick={()=>handleNavigate(tab)} key={tab}
+        className={`${active===tab?"bg-amber-500":"bg-blue-800 "} p-2`}>{tab}</button>
+      })}
     </div>
   );
 }
