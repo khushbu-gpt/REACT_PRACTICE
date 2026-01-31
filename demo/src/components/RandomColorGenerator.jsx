d0e5f9185adba8b6d6b3ede69653002fe24e6994
khushbu kumari <khushbugpt212005@gmail.com>
2026-02-01 00:16:13 +0530

    feat: enhance RandomColorGenerator with dynamic color generation and styling

--- demo/src/components/RandomColorGenerator.jsx
+++ demo/src/components/RandomColorGenerator.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const RandomColorGenerator = () => {
-    const [color,setColor]=useState("")
-    function colorGenerator() {
-       setColor()
-    }
+  const [color, setColor] = useState("blue");
+  function colorGenerator() {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setColor(randomColor);
+  }
+  useEffect(() => {
+    console.log(color);
+  },[color]);
   return (
-    <div className={`bg-${color}`}>
-      <button onClick={colorGenerator}></button>
+    <div style={{backgroundColor:color}} className="h-[100vh]">
+      <button onClick={colorGenerator} className="bg-white rounded-xl p-3">Generator</button>
     </div>
-  )
-}
+  );
+};
 
-export default RandomColorGenerator
+export default RandomColorGenerator;
